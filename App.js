9ad2bf23b5e9d3bd668af6a1a8d6207e6046df2f
pspag85 <pspag85@gmail.com>
2019-02-07 14:27:53 -0500

    chore: welcome page

--- App.js
+++ App.js
@@ -18,11 +18,11 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component(props) {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to Cart Wizard!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
